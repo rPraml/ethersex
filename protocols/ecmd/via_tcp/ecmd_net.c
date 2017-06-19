dcf233c238a5fa02014dd415af281b76eb3cbf0b
Roland Praml <pram@gmx.de>
2017-06-19 23:16:53 +0200

    some magic with new lines?

--- protocols/ecmd/via_tcp/ecmd_net.c
+++ protocols/ecmd/via_tcp/ecmd_net.c
@@ -250,7 +250,7 @@ void ecmd_net_main(void)
             }
 
             if (l > 0) {
-                state->outbuf[l++] = '\n';
+                if (state->outbuf[l] != ECMD_NO_NEWLINE) state->outbuf[l++] = '\n';
                 state->out_len = l;
             }
         }
