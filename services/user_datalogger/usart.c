15cd658b6c0068e6ec81c426a572324df2a6989a
Roland Praml <pram@gmx.de>
2022-12-17 19:38:23 +0100

    Refactor USART

--- services/user_datalogger/usart.c
+++ services/user_datalogger/usart.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <util/delay.h>
 #include <util/parity.h>
+#include <util/atomic.h>
 #include "config.h"
 #include "usart.h"
 
@@ -27,100 +28,99 @@ void (* usart_on_tx_complete)();
 
 // the receiver code
 void usart_rx_clear(void) {
-	rx_write_ptr = rx_read_ptr = 0;
+  rx_write_ptr = rx_read_ptr = 0;
 }
 
 // dies ist eine Art Ringpuffer
 int16_t
 usart_rx_put(uint8_t ch) {
-	//if (((recv_write_ptr + 1) & USART_RECV_BUFFER_MASK) == recv_read_ptr) return -1; // buffer full
-	rx_buffer[rx_write_ptr] = ch;
-	rx_write_ptr = (rx_write_ptr + 1) & USART_RX_BUFFER_MASK;
-	return 0;
+  //if (((recv_write_ptr + 1) & USART_RECV_BUFFER_MASK) == recv_read_ptr) return -1; // buffer full
+  rx_buffer[rx_write_ptr] = ch;
+  rx_write_ptr = (rx_write_ptr + 1) & USART_RX_BUFFER_MASK;
+  return 0;
 }
 
 int16_t
 usart_rx_get() {
-	if (rx_read_ptr == rx_write_ptr) return -1; // buffer empty
-	uint8_t ret = rx_buffer[rx_read_ptr];
-	rx_read_ptr = (rx_read_ptr + 1) & USART_RX_BUFFER_MASK;
-	return ret;
+  if (rx_read_ptr == rx_write_ptr) return -1; // buffer empty
+  uint8_t ret = rx_buffer[rx_read_ptr];
+  rx_read_ptr = (rx_read_ptr + 1) & USART_RX_BUFFER_MASK;
+  return ret;
 }
 
 // ISR-Routinen
 ISR(usart(USART,_RX_vect)) {
-	/* Ignore errors */
-	uint8_t flags = usart(UCSR,A);
-	if ((flags & _BV(usart(DOR))) || (usart(UCSR,A) & _BV(usart(FE)))) {
-		uint8_t v = usart(UDR);
-		(void) v;
-		return;
-	}
-	uint8_t data = usart(UDR);
-	usart_rx_put(data);
+  /* Ignore errors */
+  uint8_t flags = usart(UCSR,A);
+  if ((flags & _BV(usart(DOR))) || (usart(UCSR,A) & _BV(usart(FE)))) {
+    uint8_t v = usart(UDR);
+    (void) v;
+    return;
+  }
+  uint8_t data = usart(UDR);
+  usart_rx_put(data);
 }
 
 ISR(usart(USART,_TX_vect)) {
-	if (tx_len && tx_sent < tx_len) { // bytes in buffer that aren't sent
-		while (!(usart(UCSR,A) & _BV(usart(UDRE))));
-		usart(UDR) = tx_buffer[tx_sent++];
-	} else {
-		/* Disable this interrupt, execute callback */
-		tx_len = 0;
-		usart(UCSR, B) |= _BV(usart(RXCIE)) | _BV(usart(RXEN)); // fullduplex not yet supported (and also not needed)
-		
-		if (usart_on_tx_complete)(*usart_on_tx_complete)();
-		usart_on_tx_complete = 0;
-	}
+  if (tx_len && tx_sent < tx_len) { // bytes in buffer that aren't sent
+    while (!(usart(UCSR,A) & _BV(usart(UDRE))));
+    usart(UDR) = tx_buffer[tx_sent++];
+  } else {
+    /* Disable this interrupt, execute callback */
+    tx_len = 0;
+    usart(UCSR, B) |= _BV(usart(RXCIE)) | _BV(usart(RXEN)); // fullduplex not yet supported (and also not needed)
+    
+    if (usart_on_tx_complete)(*usart_on_tx_complete)();
+    usart_on_tx_complete = 0;
+  }
 }
 void usart_rx_start() {
-	uint8_t sreg = SREG;
-	
-	usart_rx_clear();
-	
-	cli();	
-	usart(UCSR, B) |= _BV(usart(RXCIE)) | _BV(usart(RXEN));
-	SREG = sreg;
+  usart_rx_clear();
+  
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+  {
+    usart(UCSR, B) |= _BV(usart(RXCIE)) | _BV(usart(RXEN));
+  }
 }
 
 char * usart_tx_buffer() {
-	return (char *)tx_buffer;
+  return (char *)tx_buffer;
 }
 void usart_tx_start(int16_t len, void *txc_callback) {
-	uint8_t sreg = SREG;
-	// todo check if tx_len = 0;
-	if (len <= 0) return;
-	
-	cli();
-	/* enable transmitter only, bidi not yet supported (and not needed) */
-	usart(UCSR, B) = _BV(usart(TXCIE)) | _BV(usart(TXEN));
-	/* reset tx interrupt flag */
-	usart(UCSR, A) |= _BV(usart(TXC));
-	usart_on_tx_complete = txc_callback;
-	/* Go! */
-	SREG = sreg;
+  // todo check if tx_len = 0;
+  if (len <= 0) return;
 
-	tx_len = len;
-	tx_sent = 1;
-	usart(UDR) = tx_buffer[0];	// send char
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+  {
+    /* enable transmitter only, bidi not yet supported (and not needed) */
+    usart(UCSR, B) = _BV(usart(TXCIE)) | _BV(usart(TXEN));
+    /* reset tx interrupt flag */
+    usart(UCSR, A) |= _BV(usart(TXC));
+    usart_on_tx_complete = txc_callback;
+    /* Go! */
+  }
+
+  tx_len = len;
+  tx_sent = 1;
+  usart(UDR) = tx_buffer[0];  // send char
 }
 
 void usart_setbaud(uint16_t baud, uint8_t charSize, char parity, uint8_t stopbits) {
-	uint8_t sreg = SREG;
-	cli();
-	uint8_t out;
-	switch(parity) {
-		case 'E': out |= (1 << usart(UPM,1)); break;
-		case 'O': out |= (1 << usart(UPM,1)) | (1 << usart(UPM,0)) ;
-	}
-	switch (charSize) {
-		case 7: out |= (1 << usart(UCSZ,1)) ;	break;
-		case 8: out |= (1 << usart(UCSZ,1)) | (1 << usart(UCSZ,0));	break;
-	}
-	if (stopbits == 2) out |= (1 << usart(USBS));
-	usart(UCSR, C) = _BV_URSEL | out;
-	usart(UBRR,H) = (baud >> 8);
-	usart(UBRR,L) = (baud & 0xFF); 
-	SREG = sreg;
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+  {
+    uint8_t out;
+    switch(parity) {
+      case 'E': out |= (1 << usart(UPM,1)); break;
+      case 'O': out |= (1 << usart(UPM,1)) | (1 << usart(UPM,0)) ;
+    }
+    switch (charSize) {
+      case 7: out |= (1 << usart(UCSZ,1)) ;  break;
+      case 8: out |= (1 << usart(UCSZ,1)) | (1 << usart(UCSZ,0));  break;
+    }
+    if (stopbits == 2) out |= (1 << usart(USBS));
+    usart(UCSR, C) = _BV_URSEL | out;
+    usart(UBRR,H) = (baud >> 8);
+    usart(UBRR,L) = (baud & 0xFF);
+  }
 }
 
