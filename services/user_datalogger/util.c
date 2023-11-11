39b6278e528367f7982432b63d6c3dcb639c2370
Roland Praml <pram@gmx.de>
2023-11-11 16:11:27 +0100

    Moved buffer in util.h

--- services/user_datalogger/util.c
+++ services/user_datalogger/util.c
@@ -3,21 +3,24 @@
 #include <stdlib.h>
 #include "config.h"
 #include "util.h"
-static char datalogger_scratch[70];
 
-char * util_readline(uint8_t ch, int mode) {
+char *util_readline(uint8_t ch, int mode)
+{
 	static uint8_t pos;
-    
-	if ((mode == READLINE_CRLF) && (ch == '\r')) return NULL; // ignore
-	if (((mode != READLINE_NUL) && (ch == '\n')) || 
-        ((mode == READLINE_CR)  && (ch == '\r')) ||
-        ((mode == READLINE_NUL) && (ch == 0))   ) {
-		if (!pos) return NULL;
+
+	if ((mode == READLINE_CRLF) && (ch == '\r'))
+		return NULL; // ignore
+	if (((mode != READLINE_NUL) && (ch == '\n')) ||
+		((mode == READLINE_CR) && (ch == '\r')) ||
+		((mode == READLINE_NUL) && (ch == 0)))
+	{
+		if (!pos)
+			return NULL;
 		datalogger_scratch[pos] = 0;
 		pos = 0;
 		return datalogger_scratch;
 	}
-	if (pos < 68) datalogger_scratch[pos++] = ch;
+	if (pos < DATALOGGER_SCRATCH_SIZE - 2)
+		datalogger_scratch[pos++] = ch;
 	return 0;
 }
-
